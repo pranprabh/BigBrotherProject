ab246f0eed8ec52e6d381a4d061807cf97be7edf
Kinllen Peng <kap279@cornell.edu>
2020-10-17 17:37:53 -0500

    added support for user input questions

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let allAnswers = {}
 let questionKeyArr = Object.keys(questions);
 let i = 0;
 
+// console.log(Object.keys(questions));
+// for (let ii=0; ii<questionKeyArr.length; ++ii) console.log(questionKeyArr[ii]);
+
 var questionId;
 var questionObj;
 
 function nextQuestion() {
+    console.log(i);
     if (i == questionKeyArr.length) {
         uploadAnswers();
         return;
@@ -14,30 +18,44 @@ function nextQuestion() {
     // console.log(allAnswers);
     questionId = questionKeyArr[i];
     questionObj = questions[questionId];
+    i++;
 
-    let answers;
-    if ("answers" in questionObj) answers = questionObj["answers"]
-    else answers = presets[questionObj["question-preset"]]
-
-    if (answers == null) {
-        i++;
-        nextQuestion();
-        return;
+    if (!("question-preset" in questionObj || "answers" in questionObj || "question-type" in questionObj)) {
+        return nextQuestion();
     }
 
     html = questionObj["question"];
-    html += "<form>";
-    answers.forEach(ans => {
-        html += `<input type="radio" name="formChoice" value=${ans}>${ans}</input><br>`;
-        // console.log(ans);
-    });
-    html += "</form>";
+
+    if ("question-type" in questionObj) {
+        questionType = questionObj["question-type"];
+        if (questionType == "USER_INPUT") {
+            html += "<form>";
+            html += "<input type='text' id='textInput'>"
+            html += "</form>";
+        } else return nextQuestion();
+    } else {
+        let answers;
+        if ("answers" in questionObj) answers = questionObj["answers"]
+        else answers = presets[questionObj["question-preset"]]
+
+        html += "<form>";
+        answers.forEach(ans => {
+            html += `<input type="radio" name="formChoice" value=${ans}>${ans}</input><br>`;
+        });
+        html += "</form>";
+    }
     html += "<button onClick='submitAnswer()'>Next Question</button>";
     document.getElementById("questionDiv").innerHTML = html;
-    i++;
 }
 
 function submitAnswer() {
+    if (document.getElementById("textInput")) {
+        input = document.getElementById("textInput").value;
+        if (input != "") {
+            allAnswers[questionId] = input;
+            nextQuestion();
+        }
+    }
     for (let rb of document.querySelectorAll('input[name="formChoice"]')) {
         if (rb.checked) {
             allAnswers[questionId] = rb.value;
@@ -50,8 +68,3 @@ function uploadAnswers() {
     db.collection("survey_responses").add(allAnswers);
     document.getElementById("questionDiv").innerHTML = "Thank you for completing this survey! Your response is greatly appreciated."
 }
-
-// for (let key in questions) {
-//     console.log(key, questions[key]);
-//     answers[key] = null;
-// }
